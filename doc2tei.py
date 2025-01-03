6896f75d7d73212291a0462e2c09546459764572
mzahorec <113051887+mzahorec@users.noreply.github.com>
2025-01-03 12:59:11 -0500

    Add files via upload
    Stable version before messing with namespace issue (I'll need to make some significant method changes to deal with working in the proper namespace).

--- doc2tei.py
+++ doc2tei.py
@@ -20,13 +20,23 @@ def myread(myfilename):                 ## Read lines from docx file
                 elif '\n' in listofpars[parindex].runs[runindex].text:
                     linecount+=1
             #print("P", pagecount, "Line", linecount, repr(listofpars[parindex].runs[runindex].text), listofpars[parindex].runs[runindex].bold)
-            tag = ""
-            if listofpars[parindex].runs[runindex].bold:
-                tag += "B"
-            masterlist.append([pagecount,linecount,listofpars[parindex].runs[runindex].text.replace("\n",""), tag])
             
-    for current in masterlist:
-        print(current)
+            bold = False
+            if listofpars[parindex].runs[runindex].bold:
+                bold = True
+            
+            #masterlist.append([pagecount,parindex,linecount,listofpars[parindex].runs[runindex].text.replace("\n",""), tag])
+            curdict = {
+                "page":pagecount,
+                "para":parindex,
+                "line":linecount,
+                "text":listofpars[parindex].runs[runindex].text.replace("\n",""),
+                "bold":bold
+            }
+            masterlist.append(curdict)
+            
+    #for current in masterlist:
+    #    print(current)
     
     return masterlist
     
@@ -44,8 +54,12 @@ def splitlines(mydict):                 ## Split dict contents by line and
     return dictoflists
 
 
-def genxml(listdict):                   ## generate xml structure
+def genxml():                   ## generate xml structure
     #E = lxml.builder.ElementMaker(namespace="http://www.tei-c.org/ns/1.0")
+    
+    
+    ns = "http://www.tei-c.org/ns/1.0"
+    
     E = lxml.builder.ElementMaker()
     ET = lxml.etree
     the_doc = E.TEI(
@@ -80,14 +94,16 @@ def genxml(listdict):                   ## generate xml structure
                         )
                     )
               )
-    myxml =  ET.tostring(the_doc, pretty_print=True)
+    
+    myxml =  ET.tostring(the_doc, pretty_print=False)
+    print(myxml)
     return myxml
 
 
 def popuxml(basicxml,masterlist):      ## populate xml with source data
     E = lxml.builder.ElementMaker()
-    ET = lxml.etree
-    tree = ET.fromstring(basicxml)
+    etree = lxml.etree
+    tree = etree.fromstring(basicxml)
     cnav = tree.find(".//body")
     count=0
     
@@ -103,13 +119,99 @@ def popuxml(basicxml,masterlist):      ## populate xml with source data
             cnav.append(curel)
     '''
 
-#    for item in masterlist:
-#        page =    
+
+    '''
+    prevpara = prevline = prevpage = -1
+    curtext = ""
+    for item in masterlist:
+        print(item)
+        
+        if item["page"]!=prevpage:              # make a new pb element (I *think* this only works properly
+            pbe = etree.Element("pb")           # if pb does not occur inside a <p> element)
+            pbe.set("n",str(item["page"]) )
+            cnav.append(pbe)
+            
+        if item["para"]!=prevpara:
+            if prevpara != -1:
+                cnav.append(p)
+            p = etree.Element("p")
+                           
+        if item["bold"]==True:
+            if item["line"]==prevline:
+                
+            else:
+            
+        #elif item["strike"]:
+        #elif item["note"]:
+        
+        else:
+            if item["line"]==prevline:
+                curtext+=item["text"]
+            else:
+                curtext+=item["text"]
+                lbe = etree.SubElement(p, "lb", n=str(item["line"]))
+                lbe.set("break","yes")
+                lbe.tail = curtext
+                curtext = ""
+                
+                
+        
+        prevpara = item["para"]
+        prevline = item["line"]
+        prevpage = item["page"]
+    ''' 
+    
+    prevpara = prevline = prevpage = -1
+    curtext = ""
+    for item in masterlist:
+        #print(item)
+        
+        #if item["page"]!=prevpage:              # make a new pb element (I *think* this only works properly
+        #    pbe = etree.Element("pb")           # if pb does not occur inside a <p> element)
+        #    pbe.set("n",str(item["page"]) )
+        #    cnav.append(pbe)                   THIS NEEDS FIXED!!!!!!!!!!!
+            
+        if item["para"]!=prevpara:
+            if prevpara != -1:
+                cnav.append(p)
+            p = etree.Element("p")
+            prevline = -1
+        
+        isbreak=True
+        if 'curelem' in locals():
+            if len(curtext)>1:
+                if (curtext[-1] == "-") or (curtext[-2:-1] == "- "):
+                    #print("dash ends",curtext)
+                    isbreak=False
+            
+        if item["line"]!=prevline:
+            isbreak=True
+            if 'curelem' in locals():
+                curelem.tail = curtext
+                curtext = ""
+            curelem = etree.SubElement(p, "lb", n=str(item["line"]))
+         
+        if item["bold"]==True:
+            if 'curelem' in locals():
+                curelem.tail = curtext
+                curtext = ""
+            curelem = etree.SubElement(p, "emph", rend="bold")
+            curelem.text = item["text"]
+        else:
+            curtext+=item["text"]
+
+            
+        #elif item["strike"]:
+        #elif item["note"]:
+        
+        prevpara = item["para"]
+        prevline = item["line"]
+        prevpage = item["page"]       
             
             
             
             
-    myxml = ET.tostring(tree)
+    myxml = etree.tostring(tree)
     return myxml
 
 
@@ -125,7 +227,7 @@ def main():
     #printdict(dictofpars)
     #dictoflists = splitlines(dictofpars)
     #printdict(dictoflists)
-    basicxml = genxml(dictoflists)
+    basicxml = genxml()
     populatedxml = popuxml(basicxml,masterlist)
     writexml(populatedxml,'/home/michael/Downloads/testxml04.xml')
     print("\nending program...")
